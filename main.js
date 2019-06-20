c59ee8611059bd84498e532da59cba2f86254402
Michał Krysiuk <michal.krysiuk@gmail.com>
2019-06-20 22:49:31 +0200

    feat: function in js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log('siema!');
 const name = 'Michał';
-const age = 30;
+const age = 75;
 console.log(name);
 console.log(age);
 
@@ -15,7 +15,46 @@ about.innerHTML = `Nazywam się ${name} i mam ${age} lat.`;
 
 
 const paragraphs = document.querySelectorAll('p');
-
 console.log(paragraphs);
 
-console.log(paragraphs[0]);
+if ('javascript' != 'java') {
+    console.log('to prawda!')
+}
+
+if (age < 20) {
+    console.log('masz mniej niż 20 lat');
+} else if ((age >= 20) && (age <= 29)) {
+    console.log('masz więcej niż 20 lat ale mniej niż 30');
+} else {
+    console.log('jesteś 30+');
+}
+
+switch (age) {
+    case 20:
+        console.log('masz równo 20 lat')
+        break;
+    case 30:
+        console.log('masz równo 30 lat')
+        break;
+    default:
+        console.log(`masz ${age} lat`);
+        break;
+}
+
+const amIOld = (age > 70) ? 'yes' : 'no';
+console.log(amIOld);
+
+///////
+function calculate(x) {
+    x = x + 3;
+    console.log(`tradycyjne ${x}`);
+    return x*7;
+}
+
+console.log(calculate(2));
+
+/////arrrow f
+
+const calculateFat = x => (x+3)*7;
+
+console.log(calculateFat(2));
